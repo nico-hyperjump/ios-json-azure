866a6c7ee7d9286707ee3f8957280307a444cd63
nico <nico@delightfuldev.com>
2020-06-07 23:19:15 +0200

    Initial Commit

---
+++ AzureTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AzureTestApp
+//
+//  Created by nico on 07.06.20.
+//  Copyright © 2020 nico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AzureTestAppTests/AzureTestAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  AzureTestAppTests.swift
+//  AzureTestAppTests
+//
+//  Created by nico on 07.06.20.
+//  Copyright © 2020 nico. All rights reserved.
+//
+
+import XCTest
+@testable import AzureTestApp
+
+class AzureTestAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
